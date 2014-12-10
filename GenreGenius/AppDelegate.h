fd751c348f8bace86eda683c07d65c8e94976b26
Matthew Sibson <matthew@matthewsibson.com>
2014-12-09 19:27:20 -0500

    Initial Commit

---
+++ GenreGenius/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GenreGenius
+//
+//  Created by Matthew Sibson on 12/9/14.
+//  Copyright (c) 2014 Matthew Sibson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GenreGenius/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GenreGenius
+//
+//  Created by Matthew Sibson on 12/9/14.
+//  Copyright (c) 2014 Matthew Sibson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
